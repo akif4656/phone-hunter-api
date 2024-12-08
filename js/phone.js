3039a98840a352483a2c2553ac256e17840ce34c
Akif Sarker <your-email@example.com>
2024-12-08 22:37:48 +0600

    project done

--- js/phone.js
+++ js/phone.js
@@ -15,7 +15,7 @@ const displayPhones=  phones =>{
     phones=phones.slice(0,10)
     phonecontainer.innerHTML='';
    phones.forEach(phone => {
-    //console.log(phone)
+   // console.log(phone)
     const phoneCard=document.createElement('div');
     phoneCard.classList=`card bg-base-100 shadow-xl"`;
     phoneCard.innerHTML=`
@@ -25,8 +25,8 @@ const displayPhones=  phones =>{
     <div class="card-body">
         <h2 class="card-title">${phone.phone_name}</h2>
         <p>I love coding(akif)</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+        <div class="card-actions justify-center">
+        <button onclick="showDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
         </div>
     </div>
     `
@@ -49,6 +49,27 @@ const toggleSpinner=(isLoding)=>{
    else{
     spinner.classList.add('hidden');
    }
+  
+};
+const showDetails=async(id)=>{
+    console.log(id);
+    const res=await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data=await res.json();
+    //console.log(data);
+  const phone=data.data;
+  //showDetailsData(phone);
+  showDetailsData(data.data);
+};
+const showDetailsData =(phone)=>{
+    show_details_modal.showModal();
+    console.log(phone);
+    const showName=document.getElementById('show_details_name');
+    const showDetailscontainer=document.getElementById('show_details_container');
+    showDetailscontainer.innerHTML=`
+    <p>Name:${phone.name}</p>
+    <img  src ="${phone.image}">
+    <p><span>Storage:</span>${phone.mainFeatures.storage}</p>
+    `
   
 }
 
